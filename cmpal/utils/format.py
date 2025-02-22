0df3ba3095cd420cb8b7d99197405ab035b30ada
jinyang628 <chenjinyang4192@gmail.com>
2025-02-22 14:55:13 -0500

    Print available commands

--- cmpal/utils/format.py
+++ cmpal/utils/format.py
@@ -1,5 +1,7 @@
 import re
 
+from colorama import Fore, Style
+
 
 def format_poetry_lock_diff(content: str) -> str:
     # Find all package blocks with their name and version changes
@@ -13,3 +15,14 @@ def format_poetry_lock_diff(content: str) -> str:
             formatted_blocks.append(f"poetry.lock\n{match.strip()}")
 
     return "\n\n".join(formatted_blocks)
+
+
+def print_error_message(error_message: str):
+    print(f"{Fore.RED}Error: {error_message}{Style.RESET_ALL}")
+
+
+def print_available_commands():
+    print(f"{Fore.GREEN}Available commands:{Style.RESET_ALL}")
+    print(f"{Fore.CYAN}------------------------------------{Style.RESET_ALL}")
+    print("- cmpal --setup: Set up the project with the required configurations")
+    print(f"{Fore.CYAN}------------------------------------{Style.RESET_ALL}")
